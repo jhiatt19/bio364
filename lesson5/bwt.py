7121448dc10819664174afc0d1714df92ac5e91d
jhiatt19 <hiattj19@gmail.com>
2026-07-20 17:15:47 -0600

    Solved the bwt code challenge + setup the inverse code challenge

--- lesson5/bwt.py
+++ lesson5/bwt.py
@@ -10,7 +10,19 @@ def burrows_wheeler_transform(text: str) -> str:
     """
     Generate the Burrows-Wheeler Transform of the given text.
     """
-    pass
+    array = [text]
+    j = len(array)-1
+    text_len = len(text)-1
+    for i in range(len(text)-1):
+        new_string = array[j][text_len]
+        new_string += array[j][0:text_len]
+        array.append(new_string)
+        j += 1
+    array.sort()
+    last_column = ""
+    for rotation in array:
+        last_column += rotation[-1]
+    return last_column
 
 for i in inputs:
     print(burrows_wheeler_transform(i))
---
+++ lesson5/inverse_bwt.py
@@ -0,0 +1,16 @@
+inputs = ["TTCCTAACG$A","T$ACG","AAAAAAAAAA$","TGCG$AA"]
+
+import sys
+from typing import List, Dict, Iterable, Tuple
+
+# Please do not remove package declarations because these are used by the autograder.
+
+# Insert your inverse_burrows_wheeler_transform function here, along with any subroutines you need
+def inverse_burrows_wheeler_transform(transform: str) -> str:
+    """
+    Generate the inverse of the Burrows-Wheeler Transform.
+    """
+    return transform
+
+for i in inputs:
+    print(inverse_burrows_wheeler_transform(i))
